93536803314ff014e24281cf1a911310e0c27fc6
Adnan <adnan@lakhaniproductions.com>
2020-08-07 15:39:00 -0400

    Updated Comments
    Reject project if it does not meet requirements for Exceeds Expectations

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 /******************************************
 Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
+Project 1 - A Random Quote Generator
 ******************************************/
 
 // For assistance: 
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/****
+ * AIMING TO EXCEED EXPECTATIONS. PLEASE REJECT PROJECT IF ALL REQUIREMENTS NOT MET
+ ***/
 
 /*** 
  * `quotes` array 
@@ -73,6 +77,7 @@ function randomValue() {
 /***
  * generates a random RGB color by calling the randomValue function above
  ***/
+
 function randomRGB(value) {
   const color =`rgb(${value()},${value()},${value()} )`;
   return color;
