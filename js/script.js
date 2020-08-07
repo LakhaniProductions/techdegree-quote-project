3bc9b12c7db0da9381ea50b6c3d4fb72d5bb898c
Adnan <adnan@lakhaniproductions.com>
2020-08-06 21:01:18 -0400

    Added Array of Quote Objects

--- js/script.js
+++ js/script.js
@@ -11,7 +11,54 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+const quotes = [
+  {
+    quote: `Dags, d'ya like dags?`,
+    source: `Mickey`,
+    citation: 'Snatch',
+    year: 2001,
+    tag:'Motion Picture',
+    actor:' Brad Pitt'
+  },
+  {
+    quote: `May the wind always be at your back and the sun upon your face. And may the wings of destiny carry you aloft to dance with the stars.`,
+    source: 'George Jung',
+    citation: 'Blow',
+    year: 2001,
+    tag:'Motion Picture',
+    actor:'Johnny Depp'
+  },
+  {
+    quote: `Of all forms of caution, caution in love is perhaps the most fatal to true happiness.`,
+    source: `Betrand Russell`,
+    citation: 'The Conquest of Happiness',
+    year: 1930,
+    tag:'Novel',
+    publisher: 'Liveright'
+  },
+  {
+    quote: `For you, a thousand times over.`,
+    source: `Hassan`,
+    citation: 'The Kite Runner',
+    year: 2003,
+    tag:'Novel',
+    publisher:'Riverhead Books'
+  },
+  {
+    quote: `Moral of the story is: I chose a half measure when I should have gone all the way. I'll never make that mistake again. No more half measures, Walter.`,
+    source: `Mike Ehrmantraut`,
+    citation: 'Breaking Bad',
+    year: 2010,
+    tag:'Television Series',
+    actor:'Jonathan Banks'
+  }
+  
+  
 
+]
+
+console.log(quotes[0].quote);
+console.log(quotes[4].source);
 
 /***
  * `getRandomQuote` function
@@ -30,4 +77,4 @@ project 1 - A Random Quote Generator
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// document.getElementById('load-quote').addEventListener("click", printQuote, false);
